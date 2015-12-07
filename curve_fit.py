1bd9e8ad668b3ae9fd1eec2c154d3c9c70e21e38
clr7387 <clr7387@rit.edu>
2015-12-07 12:52:50 -0500

    Started confidence interval

--- curve_fit.py
+++ curve_fit.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
+import math
 """
 Final project for Stats 251
 
@@ -12,7 +12,7 @@ Authors:    Andrew Carpenter
 """
 
 xvals = np.linspace(-1, 1, 20)
-fn0 = lambda x: 0.1*x
+fn0 = lambda x: x*5
 fn1 = lambda x: 3*np.ones(len(xvals)) #sort of like a function
 fn2 = lambda x: -2+3.*x
 fn3 = lambda x:3*x**2
@@ -27,8 +27,17 @@ def bestFit(xdata, ydata):
     polynomial=np.poly1d(coefficients)
     return polynomial(xdata)
 
-def makeFakeData(fn, data):
+def makeFakeData(fn, m,b ):
     return fn(data)+np.random.normal(size=len(data))
+
+def cofInt(xdata,m,b):
+    estx=[x*m+b for x in xdata]
+    #need tocalculate variance
+
+    upper=[x + 1.644853*math.sqrt(var) for x in estx]
+    lower=[x - 1.644853*math.sqrt(var) for x in estx]
+    plt.plot(xdata,lower)
+    plt.plot(xdata,upper)
 
 def main():
     with open("classB7.dat", "r") as ins:
@@ -47,6 +56,9 @@ def main():
     # print(np.linspace(-1, 1, 20))
     # print(fn0(ydata)+np.random.normal(size=len(ydata)))
     # print(fn0funct(ydata)+np.random.normal(size=len(ydata)))
+
+    #m,b=coefficients=np.polyfit(xdata, ydata, 1)
+    #cofInt(xdata,m,b)
 
     plt.plot(xdata, ydata, 'ko')
 
